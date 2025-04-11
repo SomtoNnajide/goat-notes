a215be5ad51e6d36bcce9a6489063f1ee669e510
SomtoNnajide <somtochukwunnajide@gmail.com>
2025-04-11 11:32:28 +1000

    Added shadCN UI, Dark mode and Toast(Sonner)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "GOAT Notes"
@@ -11,10 +13,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange 
+        >
+          {children}
+
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
