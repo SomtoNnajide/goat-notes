5359a8c866068797eba8d28da38da561a5cde892
SomtoNnajide <somtochukwunnajide@gmail.com>
2025-04-11 10:27:12 +1000

    Project configuration, added Prettier dependency, added esLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off" //allow the use of apostrophes
+    }
+  }
 ];
 
 export default eslintConfig;
