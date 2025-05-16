325eeb5c75199cc61b0c5592f18c0b370077790a
SomtoNnajide <somtochukwunnajide@gmail.com>
2025-05-16 16:47:40 +1000

    Finished NewNoteButton ... the engineering is peaking lads

--- src/components/NewNoteButton.tsx
+++ src/components/NewNoteButton.tsx
@@ -1,16 +1,53 @@
 'use client'
 
 import { User } from "@supabase/supabase-js"
+import { Button } from "./ui/button"
+import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
+import { toast } from "sonner"
+import { createNoteAction } from "@/actions/notes"
 
 type Props = {
     user: User | null
 }
 
 function NewNoteButton({ user }: Props) {
-    console.log(user?.email)
+    const router = useRouter()
+    const [loading, setLoading] = useState(false)
+
+    const handleClickNewNoteButton = async () => {
+        if(!user){
+            router.push("/login")
+        } else {
+            setLoading(true)
+
+            const uuid = uuidv4()
+            await createNoteAction(uuid)
+            router.push(`/?noteId=${uuid}`)
+
+            toast.success("New Note Created", {
+                style: {
+                    background: "var(--color-success-bg)",
+                    color: "var(--color-success-text)",
+                },
+                description: "You have created a new note"
+            })
+
+            setLoading(false)
+        }
+    }
 
     return (
-        <div>NewNoteButton</div>
+        <Button
+        onClick={handleClickNewNoteButton}
+        variant='secondary'
+        className="w-24"
+        disabled={loading}
+        >
+            {loading ? <Loader2 className="animate-spin" /> : "New Note"}
+        </Button>
     )
 }
 
