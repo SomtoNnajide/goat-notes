372f465eea64fafd6ee76e6a4d3d2175f7bd184d
SomtoNnajide <somtochukwunnajide@gmail.com>
2025-05-20 12:38:53 +1000

    json update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "react/no-unescaped-entities": "off" //allow the use of apostrophes
+      "react/no-unescaped-entities": "off", 
     }
   }
 ];
